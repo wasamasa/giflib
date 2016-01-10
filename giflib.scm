5c31a9c3982703c0bbfaa369933199cc0aaa20c0
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-01-10 19:40:11 +0100

    Define a giflib error condition

--- giflib.scm
+++ giflib.scm
@@ -11,66 +11,58 @@
 (define GIF-ERROR (foreign-value "GIF_ERROR" int))
 (define GIF-OK (foreign-value "GIF_OK" int))
 
-;; TODO: create a new error condition
-(define (gif-error-string status)
-  ((foreign-lambda c-string "GifErrorString" int)
-   status))
+(define (gif-error status)
+  (abort
+   (make-composite-condition
+    (make-property-condition
+     'exn
+     'message ((foreign-lambda c-string "GifErrorString" int)
+               status))
+    (make-property-condition
+     'giflib
+     'code status))))
 
 ;; TODO: find some way to clean up repetitive code
 (define (close-gif gif)
   (let ((gif* (gif-pointer gif)))
     (when gif*
       (let-location ((status int 0))
-        (let ((ret ((foreign-lambda int
-                                    "DGifCloseFile"
-                                    (c-pointer (struct "GifFileType"))
-                                    (c-pointer int))
+        (let ((ret ((foreign-lambda int "DGifCloseFile" (c-pointer (struct "GifFileType")) (c-pointer int))
                     gif* (location status))))
           (when (= ret GIF-ERROR)
-            (error (gif-error-string status)))))
+            (gif-error status))))
       (gif-pointer-set! gif #f))))
 
 (define (open-gif filename)
   (let-location ((status int 0))
-    (let ((gif* ((foreign-lambda (c-pointer (struct "GifFileType"))
-                                 "DGifOpenFileName"
-                                 c-string
-                                 (c-pointer int))
+    (let ((gif* ((foreign-lambda (c-pointer (struct "GifFileType")) "DGifOpenFileName" c-string (c-pointer int))
                  filename (location status))))
       (if gif*
           (set-finalizer! (make-gif gif*) close-gif)
-          (error (gif-error-string status))))))
+          (gif-error status)))))
 
 (define (slurp-gif gif)
   (let ((gif* (gif-pointer gif)))
     (when gif*
-      (let ((ret ((foreign-lambda int
-                                  "DGifSlurp"
-                                  (c-pointer (struct "GifFileType")))
+      (let ((ret ((foreign-lambda int "DGifSlurp" (c-pointer (struct "GifFileType")))
                   gif*)))
         (when (= ret GIF-ERROR)
-          (let ((status ((foreign-lambda* int
-                                          (((c-pointer (struct "GifFileType"))
-                                            gif))
+          (let ((status ((foreign-lambda* int (((c-pointer (struct "GifFileType")) gif))
                                           "C_return(gif->Error);")
                          gif*)))
-            (error (gif-error-string status))))))))
+            (gif-error status)))))))
 
 (define (gif-width gif)
   (let ((gif* (gif-pointer gif)))
     (when gif*
-      ((foreign-lambda* int
-                        (((c-pointer (struct "GifFileType"))
-                          gif))
+      ((foreign-lambda* int (((c-pointer (struct "GifFileType")) gif))
                         "C_return(gif->SWidth);")
        gif*))))
 
 (define (gif-height gif)
   (let ((gif* (gif-pointer gif)))
     (when gif*
-      ((foreign-lambda* int
-                        (((c-pointer (struct "GifFileType"))
-                          gif))
+      ((foreign-lambda* int (((c-pointer (struct "GifFileType")) gif))
                         "C_return(gif->SHeight);")
        gif*))))
 )

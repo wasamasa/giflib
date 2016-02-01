e14f08440117ab2119b6b485baefa5925689eee5
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-02-01 14:16:21 +0100

    Use more canonical names

--- giflib.scm
+++ giflib.scm
@@ -56,17 +56,17 @@
 
 ;; GifFileType
 (define GifFileType->SWidth (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->SWidth);"))
-(define GifFileType->SWidth! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int width)) "gif->SWidth = width;"))
+(define GifFileType->SWidth-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int width)) "gif->SWidth = width;"))
 (define GifFileType->SHeight (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->SHeight);"))
-(define GifFileType->SHeight! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int height)) "gif->SHeight = height;"))
+(define GifFileType->SHeight-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int height)) "gif->SHeight = height;"))
 (define GifFileType->SColorResolution (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->SColorResolution);"))
-(define GifFileType->SColorResolution! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int resolution)) "gif->SColorResolution = resolution;"))
+(define GifFileType->SColorResolution-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int resolution)) "gif->SColorResolution = resolution;"))
 (define GifFileType->SBackGroundColor (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->SBackGroundColor);"))
-(define GifFileType->SBackGroundColor! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int index)) "gif->SBackGroundColor = index;"))
+(define GifFileType->SBackGroundColor-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (int index)) "gif->SBackGroundColor = index;"))
 (define GifFileType->AspectByte (foreign-lambda* unsigned-byte (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->AspectByte);"))
-(define GifFileType->AspectByte! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (unsigned-byte ratio)) "gif->AspectByte = ratio;"))
+(define GifFileType->AspectByte-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) (unsigned-byte ratio)) "gif->AspectByte = ratio;"))
 (define GifFileType->SColorMap (foreign-lambda* (c-pointer (struct "ColorMapObject")) (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->SColorMap);"))
-(define GifFileType->SColorMap! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) ((c-pointer (struct "ColorMapObject")) color_map)) "gif->SColorMap = color_map;"))
+(define GifFileType->SColorMap-set! (foreign-lambda* void (((c-pointer (struct "GifFileType")) gif) ((c-pointer (struct "ColorMapObject")) color_map)) "gif->SColorMap = color_map;"))
 (define GifFileType->ImageCount (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->ImageCount);"))
 (define GifFileType->SavedImage (foreign-lambda* (c-pointer (struct "SavedImage")) (((c-pointer (struct "GifFileType")) gif) (int i)) "C_return(&(gif->SavedImages[i]));"))
 (define GifFileType->ExtensionBlockCount (foreign-lambda* int (((c-pointer (struct "GifFileType")) gif)) "C_return(gif->ExtensionBlockCount);"))
@@ -77,20 +77,20 @@
 (define ColorMapObject->ColorCount (foreign-lambda* int (((c-pointer (struct "ColorMapObject")) color_map)) "C_return(color_map->ColorCount);"))
 (define ColorMapObject->BitsPerPixel (foreign-lambda* int (((c-pointer (struct "ColorMapObject")) color_map)) "C_return(color_map->BitsPerPixel);"))
 (define ColorMapObject->SortFlag (foreign-lambda* bool (((c-pointer (struct "ColorMapObject")) color_map)) "C_return(color_map->SortFlag);"))
-(define ColorMapObject->SortFlag! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (bool flag)) "color_map->SortFlag = flag;"))
+(define ColorMapObject->SortFlag-set! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (bool flag)) "color_map->SortFlag = flag;"))
 (define ColorMapObject->Color (foreign-lambda* (c-pointer (struct "GifColorType")) (((c-pointer (struct "ColorMapObject")) color_map) (int i)) "C_return(&(color_map->Colors[i]));"))
 ;; TODO: define create-color! for passing the result to this
 ;; TODO: put this on heap and free with finalizer?
-(define ColorMapObject->Color! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (int i) ((c-pointer (struct "GifColorType")) color)) "color_map->Colors[i] = *color;"))
-(define ColorMapObject->Color*! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (int i) (unsigned-byte red) (unsigned-byte green) (unsigned-byte blue)) "color_map->Colors[i] = (GifColorType) { red, green, blue };"))
+(define ColorMapObject->Color-set! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (int i) ((c-pointer (struct "GifColorType")) color)) "color_map->Colors[i] = *color;"))
+(define ColorMapObject->Color*-set! (foreign-lambda* void (((c-pointer (struct "ColorMapObject")) color_map) (int i) (unsigned-byte red) (unsigned-byte green) (unsigned-byte blue)) "color_map->Colors[i] = (GifColorType) { red, green, blue };"))
 
 ;; GifColorType
 (define GifColorType->Red (foreign-lambda* unsigned-byte (((c-pointer (struct "GifColorType")) color)) "C_return(color->Red);"))
-(define GifColorType->Red! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte red)) "color->Red = red;"))
+(define GifColorType->Red-set! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte red)) "color->Red = red;"))
 (define GifColorType->Green (foreign-lambda* unsigned-byte (((c-pointer (struct "GifColorType")) color)) "C_return(color->Green);"))
-(define GifColorType->Green! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte green)) "color->Green = green;"))
+(define GifColorType->Green-set! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte green)) "color->Green = green;"))
 (define GifColorType->Blue (foreign-lambda* unsigned-byte (((c-pointer (struct "GifColorType")) color)) "C_return(color->Blue);"))
-(define GifColorType->Blue! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte blue)) "color->Blue = blue;"))
+(define GifColorType->Blue-set! (foreign-lambda* void (((c-pointer (struct "GifColorType")) color) (unsigned-byte blue)) "color->Blue = blue;"))
 
 ;; ExtensionBlock
 (define ExtensionBlock->Function (foreign-lambda* int (((c-pointer (struct "ExtensionBlock")) extension_block)) "C_return(extension_block->Function);"))
@@ -101,23 +101,23 @@
 
 ;; SavedImage
 (define SavedImage->Width (foreign-lambda* int (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.Width);"))
-(define SavedImage->Width! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int width)) "frame->ImageDesc.Width = width;"))
+(define SavedImage->Width-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int width)) "frame->ImageDesc.Width = width;"))
 (define SavedImage->Height (foreign-lambda* int (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.Height);"))
-(define SavedImage->Height! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int height)) "frame->ImageDesc.Height = height;"))
+(define SavedImage->Height-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int height)) "frame->ImageDesc.Height = height;"))
 (define SavedImage->Left (foreign-lambda* int (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.Left);"))
-(define SavedImage->Left! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int left)) "frame->ImageDesc.Left = left;"))
+(define SavedImage->Left-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int left)) "frame->ImageDesc.Left = left;"))
 (define SavedImage->Top (foreign-lambda* int (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.Top);"))
-(define SavedImage->Top! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int top)) "frame->ImageDesc.Top = top;"))
+(define SavedImage->Top-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int top)) "frame->ImageDesc.Top = top;"))
 (define SavedImage->Interlace (foreign-lambda* bool (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.Interlace);"))
-(define SavedImage->Interlace! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (bool interlace)) "frame->ImageDesc.Interlace = interlace;"))
+(define SavedImage->Interlace-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (bool interlace)) "frame->ImageDesc.Interlace = interlace;"))
 (define SavedImage->ColorMap (foreign-lambda* (c-pointer (struct "ColorMapObject")) (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ImageDesc.ColorMap);"))
-(define SavedImage->ColorMap! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) ((c-pointer (struct "ColorMapObject")) color_map)) "frame->ImageDesc.ColorMap = color_map;"))
+(define SavedImage->ColorMap-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) ((c-pointer (struct "ColorMapObject")) color_map)) "frame->ImageDesc.ColorMap = color_map;"))
 (define SavedImage->ExtensionBlockCount (foreign-lambda* int (((c-pointer (struct "SavedImage")) frame)) "C_return(frame->ExtensionBlockCount);"))
 (define SavedImage->ExtensionBlock (foreign-lambda* (c-pointer (struct "ExtensionBlock")) (((c-pointer (struct "SavedImage")) frame) (int i)) "C_return(&(frame->ExtensionBlocks[i]));"))
 (define SavedImage->pixel (foreign-lambda* unsigned-byte (((c-pointer (struct "SavedImage")) frame) (int width) (int x) (int y)) "C_return(frame->RasterBits[y*width+x]);"))
-(define SavedImage->pixel! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int width) (int x) (int y) (int color)) "frame->RasterBits[y*width+x] = color;"))
+(define SavedImage->pixel-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (int width) (int x) (int y) (int color)) "frame->RasterBits[y*width+x] = color;"))
 (define SavedImage->row (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (u8vector dest) (int width) (int row)) "memcpy(dest, frame->RasterBits + row * width, width * sizeof(unsigned char));"))
-(define SavedImage->row! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (u8vector src) (int width) (int row)) "memcpy(frame->RasterBits + row * width, src, width * sizeof(unsigned char));"))
+(define SavedImage->row-set! (foreign-lambda* void (((c-pointer (struct "SavedImage")) frame) (u8vector src) (int width) (int row)) "memcpy(frame->RasterBits + row * width, src, width * sizeof(unsigned char));"))
 
 ;;; auxiliary records
 
